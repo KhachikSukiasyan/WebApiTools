fba6a12f4f7b9c7603860a9a1cd9c181e157ebc6
aghasilorsabyanmic <aghasi.lorsabyan@mic.am>
2016-03-07 15:13:48 +0400

    Added WebApi project.

---
+++ WebApi.Tools.ImageGenerator/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.Tools.ImageGenerator.Startup))]
+
+namespace WebApi.Tools.ImageGenerator
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage("/");
+
+            HttpConfiguration httpConfig = new HttpConfiguration();
+
+            ConfigureWebApi(httpConfig);
+
+            app.UseWebApi(httpConfig);
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
